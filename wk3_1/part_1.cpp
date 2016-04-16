04b0fb94f445d7eda15b3bc76b390e2f40b4366e
Wei-Lun CHANG <maomaoto@gmail.com>
2016-04-16 16:19:32 +0800

    update wk3-1

--- wk3_1/part_1.cpp
+++ wk3_1/part_1.cpp
@@ -4,7 +4,19 @@ class A {
 public:
     int val;
 // 在此处补充你的代码
+    A();
+    int & GetObj();
 };
+
+A::A(){
+    val = 0;
+}
+
+int & A::GetObj(){
+    //int & r = val;
+    return val;
+}
+
 main()  {
     A a;
     cout << a.val << endl;
