4b3e86dad5ed8c11a2088d5b190ee116f8138f70
maomaoto_office <maomaoto+office@gmail.com>
2016-04-18 14:36:01 +0800

    update wk 3-4

--- wk3_4/wk3_4.cpp
+++ wk3_4/wk3_4.cpp
@@ -3,6 +3,7 @@
 */
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 enum KnightType{dragon, ninja, iceman, lion, wolf};
@@ -24,6 +25,7 @@ public:
 
 };
 
+
 class HeadQuarter {
 public:
     HQType type;
@@ -31,12 +33,17 @@ public:
     int TotalKnight;
     int KnightCount[5];
     KnightType ProduceSeq[5];
+    int ProduceTick;
+    static int KnightLife[5];
+    int timer;
+
 
     HeadQuarter(HQType t, int l): type(t), life(l) {
         TotalKnight = 0;
         for (int i = 0; i<5; i++){
             KnightCount[i] = 0;
         }
+
         if (t == red) {
             ProduceSeq[0] = iceman;
             ProduceSeq[1] = lion;
@@ -51,27 +58,74 @@ public:
             ProduceSeq[4] = wolf;
         }
 
+        ProduceTick = 0;
+        timer = 0;
+
+    }
+
+    bool ProduceKnight() {
+        // return false if stop making warrior
+        int tempTick = ProduceTick;
+        while(true) {
+            if (life >= KnightLife[ProduceSeq[ProduceTick]]) {
+                TotalKnight++;
+                KnightCount[ProduceSeq[ProduceTick]]++;
+                life -= KnightLife[ProduceSeq[ProduceTick]];
+                cout << setfill('0') << setw(3) << timer << ' ';
+                cout << HQName[type] << ' ' << KnightName[ProduceSeq[ProduceTick]] << ' ';
+                cout << TotalKnight << " born with strength " << KnightLife[ProduceSeq[ProduceTick]] << ',';
+                cout << KnightCount[ProduceSeq[ProduceTick]] << ' ' << KnightName[ProduceSeq[ProduceTick]];
+                cout << " in " << HQName[type] << " headquarter" << endl;
+                timer++;
+                ProduceTick = (ProduceTick + 1) % 5;
+                return true;
+            } else {
+                ProduceTick = (ProduceTick + 1) % 5;
+                if (tempTick == ProduceTick) {
+                    cout << setfill('0') << setw(3) << timer << ' ';
+                    cout << HQName[type] << "  headquarter stops making warriors" << endl;
+                    return false;
+                }
+            }
+        }
+
+
     }
 
 };
 
+int HeadQuarter::KnightLife[5] = {0, 0, 0, 0, 0};
 
 int main()  {
 
     int NumDataSet = 0;
     int HQLife = 0;
-    int KnightLife[5] = {0};
+    //int KnightLife[5] = {0};
 
 
     cin >> NumDataSet;
 
     for (int i = 0; i< NumDataSet; i++){
         cin >> HQLife;
-        cin >> KnightLife[dragon];
-        cin >> KnightLife[ninja];
-        cin >> KnightLife[iceman];
-        cin >> KnightLife[lion];
-        cin >> KnightLife[wolf];
+        cin >> HeadQuarter::KnightLife[dragon];
+        cin >> HeadQuarter::KnightLife[ninja];
+        cin >> HeadQuarter::KnightLife[iceman];
+        cin >> HeadQuarter::KnightLife[lion];
+        cin >> HeadQuarter::KnightLife[wolf];
+
+        HeadQuarter HQRed(red, HQLife);
+        HeadQuarter HQBlue(blue, HQLife);
+
+        bool RedFlag = true;
+        bool BlueFlag = true;
+
+        cout << "Case:" << i+1 << endl;
+        while (RedFlag | BlueFlag) {
+            if (RedFlag) RedFlag = HQRed.ProduceKnight();
+            if (BlueFlag) BlueFlag = HQBlue.ProduceKnight();
+        }
+
+
     }
 
 
