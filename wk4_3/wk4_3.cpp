4b88983a9224d8cfb74ede903ce043700e400817
maomaoto_office <maomaoto+office@gmail.com>
2016-04-20 14:03:06 +0800

    update wk 4-3

--- wk4_3/wk4_3.cpp
+++ wk4_3/wk4_3.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <cstring>
-using namespace std;
-// 在此处补充你的代码
 class Array2 {
 private:
     int ** array2d;
@@ -78,27 +74,3 @@ public:
     }
 
 };
-
-
-int main() {
-    Array2 a(3,4);
-    int i,j;
-    for(  i = 0;i < 3; ++i )
-        for(  j = 0; j < 4; j ++ )
-            a[i][j] = i * 4 + j;
-    for(  i = 0;i < 3; ++i ) {
-        for(  j = 0; j < 4; j ++ ) {
-            cout << a(i,j) << ",";
-        }
-        cout << endl;
-    }
-    cout << "next" << endl;
-    Array2 b;     b = a;
-    for(  i = 0;i < 3; ++i ) {
-        for(  j = 0; j < 4; j ++ ) {
-            cout << b[i][j] << ",";
-        }
-        cout << endl;
-    }
-    return 0;
-}
