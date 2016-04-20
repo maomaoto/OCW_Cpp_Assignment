72d5fe0264b84f59c8f96941ff63b7d7d3fcdc0a
maomaoto_office <maomaoto+office@gmail.com>
2016-04-20 14:02:30 +0800

    update wk4-1

--- wk4_1/part 1.cpp
+++ wk4_1/part 1.cpp
@@ -10,10 +10,30 @@ public:
         cout << r << "+" << i << "i" << endl;
     }
 // 在此处补充你的代码
+    char * operator=(const char * s){
+        if (s) {
+            //r = 0;
+            //i = 0;
+            char * pchPlus = strchr(s, '+');
+            char * pchEnd = strchr(s, 'i');
+            // only consider good input...
+            if ( (pchPlus != NULL) & (pchEnd != NULL)){
+                char strReal[pchPlus-s+1];
+                strncpy(strReal, s, pchPlus-s);
+                strReal[pchPlus - s] = '\0';
+                r = atof(strReal);
+                char strImg[pchEnd - pchPlus -1];
+                strncpy(strImg, pchPlus+1, pchEnd - pchPlus-1);
+                strImg[pchEnd - pchPlus -1] = '\0';
+                i = atof(strImg);
+            }
+        }
+        return (char*)s;
+    }
 };
 int main() {
     Complex a;
-    a = "3+4i"; a.Print();
+    a = "3.5+4.6i"; a.Print();
     a = "5+6i"; a.Print();
     return 0;
 }
---
+++ wk4_1/wk4_1.cpp
@@ -0,0 +1,20 @@
+char * operator=(const char * s){
+	if (s) {
+		//r = 0;
+		//i = 0;
+		char * pchPlus = strchr(s, '+');
+		char * pchEnd = strchr(s, 'i');
+		// only consider good input...
+		if ( (pchPlus != NULL) & (pchEnd != NULL)){
+			char strReal[pchPlus-s+1];
+			strncpy(strReal, s, pchPlus-s);
+			strReal[pchPlus - s] = '\0';
+			r = atof(strReal);
+			char strImg[pchEnd - pchPlus -1];
+			strncpy(strImg, pchPlus+1, pchEnd - pchPlus-1);
+			strImg[pchEnd - pchPlus -1] = '\0';
+			i = atof(strImg);
+		}
+	}
+	return (char*)s;
+}
