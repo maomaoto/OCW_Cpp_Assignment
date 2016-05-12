761e8803dce09dfb20fe6484947d215ebc2cbb19
maomaoto_office <maomaoto+office@gmail.com>
2016-05-12 17:26:47 +0800

    update wk5-1

--- wk5_1/part_1.cpp
+++ wk5_1/part_1.cpp
@@ -9,18 +9,41 @@ public:
     MyString(): string() {}
     MyString(const char *s): string(s) {}
     MyString(const MyString& mstr): string(mstr) {}
+    MyString(const string& str): string(str){}
 
     MyString& operator= (const MyString& mstr){
         ((string *) this)->operator=(mstr);
         return *this;
     }
 
-    MyString& operator+ (const MyString& mstr) {
-        ((string *) this)->append(mstr);
-        return *this;
+    MyString operator+ (const MyString& mstr) {
+        MyString temp(*this);
+        temp.append(mstr);
+        return temp;
     }
 
+    MyString operator+ (const char* s) {
+        MyString temp(*this);
+        temp.append(s);
+        return temp;
+    }
+
+    MyString operator() (int pos, int len){
+        MyString mstr = ((string *) this)->substr(pos, len);
+        return mstr;
+    }
+
+    friend MyString operator+(char* s, const MyString& mstr);
+
+
+
 };
+
+MyString operator+(char* s, const MyString& mstr){
+    MyString temp(s);
+    temp = temp + mstr;
+    return temp;
+}
 
 int CompareString( const void * e1, const void * e2) {
     MyString * s1 = (MyString * ) e1;
